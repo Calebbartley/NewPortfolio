9d0ac3ae6fb9a7df15ca7c423bffd943dd2e9074
Caleb Bartley <cajo9@Server6.attlocal.net>
2023-02-28 09:13:52 -0500

    Asset and sound folder added'

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,14 +1,20 @@
 
 import './App.css';
+import redpyramid from"./assets/images/redpyramid.jpg";
 
 import React from 'react'
 
 function App() {
   return (
-    <div>
+    <div className='App-header' style={{backgroundImage:`url(${redpyramid})`}}>
       <h1>
-        HELLO WORLD!
+        HEY! I'm Caleb Bartley.
       </h1>
+      <div>
+        <h4>
+          I'm a Full-Stack Developer
+        </h4>
+      </div>
     </div>
   )
 }
